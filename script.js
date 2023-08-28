58d40f5282854286d61ce9d4ac3a8b4cd8e1bdde
Nova era <33063860+novaera2023@users.noreply.github.com>
2023-08-28 00:11:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#typed', {
+    strings: ['Anki e ChatGPT', 'AnkiGPT'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    // fadeOut: true,
+    loop: true,
+    loopCount: 1,
+    // showCursor: false
+    // cursorChar: '...'
+
+});
